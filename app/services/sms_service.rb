bf88b50585903d5f577937a4238805286eb36910
Andrew Tinekov <allexus39@gmail.com>
2020-07-13 11:14:00 +0700

    Add SMS service

---
+++ app/services/sms_service.rb
@@ -0,0 +1,27 @@
+Addclass SMSService
+  include HTTParty
+  base_uri 'https://sms.ru'
+
+  def initialize(message, user)
+    @message = message
+    @user = user
+  end
+
+  def call
+    @options = { query: { api_id: api_id msg: message, to: user.phone_number, json: 1 }}
+
+    send_message
+  end
+
+  private
+
+  attr_reader :message, :options, :user
+
+  def api_id
+    Rails.application.credentials.smsru[:api_id]
+  end
+
+  def send_message
+    self.class.get('/sms/send', options)
+  end
+end
