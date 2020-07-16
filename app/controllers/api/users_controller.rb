b44e37aa580f6fac68dda7d2be9acfd4843b034d
Andrew Tinekov <allexus39@gmail.com>
2020-07-16 12:28:07 +0700

    Render json in users controller

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -5,16 +5,16 @@ class API::UsersController < APIController
     User.create_or_find_by!(phone_number: params[:phone_number]) do |user|
       user.set_new_password
     end
-    render status: :created
+    render json: { status: :created }, status: :created
   rescue ActiveRecord::RecordNotFound, ActiveRecord::RecordNotSaved
-    render status: :unprocessable_entity
+    render json: { status: :unprocessable_entity }, status: :unprocessable_entity
   end
 
   def update
     current_user.update!(user_params)
-    render status: :ok
+    render json: { status: :ok }, status: :ok
   rescue ActiveRecord::RecordNotSaved
-    render status: :unprocessable_entity
+    render json: { status: :unprocessable_entity }, status: :unprocessable_entity
   end
 
   private
