9353924fe32738066d634a92317f2ddd669f8a99
Andrew Tinekov <allexus39@gmail.com>
2020-07-16 12:24:11 +0700

    Fix missing ':' in users controllers

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -5,16 +5,16 @@ class API::UsersController < APIController
     User.create_or_find_by!(phone_number: params[:phone_number]) do |user|
       user.set_new_password
     end
-    render status :created
+    render status: :created
   rescue ActiveRecord::RecordNotFound, ActiveRecord::RecordNotSaved
-    render status :unprocessable_entity
+    render status: :unprocessable_entity
   end
 
   def update
     current_user.update!(user_params)
-    render status :ok
+    render status: :ok
   rescue ActiveRecord::RecordNotSaved
-    render status :unprocessable_entity
+    render status: :unprocessable_entity
   end
 
   private
