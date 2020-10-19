55a91599f92b10b78327b4d16bca48c95c205622
Andrew Tinekov <andrewt7091@gmail.com>
2020-10-19 21:24:09 +0300

    Fix message sender

--- app/services/message_sender.rb
+++ app/services/message_sender.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 module MessageSender
-  def send_message(message, user = nil)
+  def self.send_message(message, user = nil)
     MessageSender::SlackBot.new(message, user).send_message
     MessageSender::SMSRU.new(message, user).send_message
     MessageSender::Debug.new(message, user).send_message
