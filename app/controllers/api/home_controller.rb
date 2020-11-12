89a5679b335f864c914707a1ee25a55a1dfae332
Andrew Tinekov <andrewt7091@gmail.com>
2020-11-12 08:27:22 +0300

    API: Fix ordering for receipt in home controller

--- app/controllers/api/home_controller.rb
+++ app/controllers/api/home_controller.rb
@@ -2,6 +2,6 @@ class API::HomeController < APIController
   def index
     @receipt =
       current_user.receipts.includes(:item, promotion: :distribution_network)
-                  .where.not(state: :completed).order(created_at: :desc).last
+                  .where.not(state: :completed).order(created_at: :desc).first
   end
 end
