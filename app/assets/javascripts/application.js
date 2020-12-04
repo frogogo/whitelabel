80cc512ddf68142af705f7e417aaeb3a3a3fccda
Andrew Tinekov <andrewt7091@gmail.com>
2020-12-05 02:59:12 +0300

    Fix typo

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,7 +1,7 @@
 window.addEventListener("load", () => {
   const menu_button = document.querySelector(".header__burger");
   menu_button.addEventListener("click", () => {
-    document.querySelector(".haeder__mob-header").classList.toggle("haeder__mob-header_open");
+    document.querySelector(".header__mob-header").classList.toggle("header__mob-header_open");
     document.querySelector(".header__burger").classList.toggle("header__burger_open");
   });
 });
