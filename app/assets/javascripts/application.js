14949d49308351d8a0b927d50f78ceee6a0bd6a6
Andrew Tinekov <andrewt7091@gmail.com>
2020-12-05 03:27:56 +0300

    Frontend: update main script
    - use const/var where it's necessary
    - use '.className' instead of '.classToggle'
    - Add 'close menu after click on a link'

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,7 +1,18 @@
 window.addEventListener("load", () => {
-  const menu_button = document.querySelector(".header__burger");
-  menu_button.addEventListener("click", () => {
-    document.querySelector(".header__mob-header").classList.toggle("header__mob-header_open");
-    document.querySelector(".header__burger").classList.toggle("header__burger_open");
+  const menuButton = document.querySelector(".header__burger");
+  const closeMenuLinks = document.querySelectorAll(".header__mob-link");
+  var headerMob = document.querySelector(".header__mob-header");
+  var headerBurger = document.querySelector(".header__burger");
+
+  menuButton.addEventListener("click", () => {
+    headerMob.className = "header__mob-header header__mob-header_open";
+    headerBurger.className = "header__burger header__burger_open";
+  });
+
+  closeMenuLinks.forEach((link) => {
+    link.addEventListener("click", () => {
+      headerMob.className = "header__mob-header";
+      headerBurger.className = "header__burger";
+    });
   });
 });
