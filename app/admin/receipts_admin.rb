5ced0295803109af9b21b68bf005bcbd92d85a86
Andrew Tinekov <andrewt7091@gmail.com>
2020-12-07 00:00:18 +0300

    Change 'text_field' to 'text_area'

--- app/admin/receipts_admin.rb
+++ app/admin/receipts_admin.rb
@@ -41,7 +41,7 @@ Trestle.resource(:receipts) do
     select :reject_reason, Receipt.reject_reasons.keys, include_blank: true
     select :promotion_id, Promotion.all, include_blank: true
     select :item_id, Item.all, include_blank: true
-    text_field :data, disabled: true
+    text_area :data, disabled: true
   end
 
   params do |params|
