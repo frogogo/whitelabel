0ed9c7649fcb2da4449cb04ebfe6fb1666cfe764
Dan Kim <itsdanya@gmail.com>
2021-05-04 23:50:48 +0300

    Migrate api/users

---
+++ test/controllers/api/users_controller_test.rb
@@ -0,0 +1,27 @@
+require "test_helper"
+
+class API::UsersControllerTest < ActionDispatch::IntegrationTest
+  test 'creates user' do
+    post '/api/user',
+         params: { phone_number: '+79991113344' }.to_json,
+         headers: api_headers
+
+    assert_response :created
+  end
+
+  test 'not creates user becase of wrong phone number' do
+    post '/api/user',
+         params: { phone_number: '799911133' }.to_json,
+         headers: api_headers
+
+    assert_response :unprocessable_entity
+  end
+
+  test 'not creates user becase of wrong headers' do
+    post '/api/user',
+         params: { phone_number: '+79991113344' }.to_json,
+         headers: {}
+
+    assert_response :unprocessable_entity
+  end
+end
