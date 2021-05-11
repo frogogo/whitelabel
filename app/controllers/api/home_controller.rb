4d92f7ae49581babf73c326b980b9a8e39a5341c
Dan Kim <itsdanya@gmail.com>
2021-05-11 16:13:40 +0300

    Order receipts by descending created_at

--- app/controllers/api/home_controller.rb
+++ app/controllers/api/home_controller.rb
@@ -1,6 +1,6 @@
 class API::HomeController < APIController
   def index
-    @receipts = current_user.receipts.order(created_at: :asc)
+    @receipts = current_user.receipts.order(created_at: :desc)
     @coupons = current_user.coupons.order(created_at: :desc)
   end
 end
