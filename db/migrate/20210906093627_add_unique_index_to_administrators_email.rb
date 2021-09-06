085df521fe1f4b59cac10a937e467746e13b72ef
Polina Janochkina <apollinariya2209@yandex.ru>
2021-09-06 12:42:14 +0300

    Add uniq index to admin email

---
+++ db/migrate/20210906093627_add_unique_index_to_administrators_email.rb
@@ -0,0 +1,5 @@
+class AddUniqueIndexToAdministratorsEmail < ActiveRecord::Migration[6.1]
+  def change
+    add_index :administrators, :email, unique: true
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_29_100112) do
+ActiveRecord::Schema.define(version: 2021_09_06_093627) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 2021_07_29_100112) do
     t.datetime "remember_token_expires_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_administrators_on_email", unique: true
   end
 
   create_table "apm_campaigns", id: false, force: :cascade do |t|
